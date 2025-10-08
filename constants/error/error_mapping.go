062724fe96805ea9c4eec31202d72fcf411bed6c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-08 10:20:47 +0700

    Refactor code structure for improved readability and maintainability

--- constants/error/error_mapping.go
+++ constants/error/error_mapping.go
@@ -1,24 +1,18 @@
 package error
 
 import (
-	errField "payment-service/constants/error/field"
-	errFieldSchedule "payment-service/constants/error/fieldSchedule"
-	errTime "payment-service/constants/error/time"
+	errPayment "payment-service/constants/error/payment"
 )
 
 func ErrMapping(err error) bool {
 	var (
-		GeneralErrors       = GeneralErrors
-		FieldErrors         = errField.FieldErrors
-		FieldScheduleErrors = errFieldSchedule.FieldScheduleErrors
-		TimeErrors          = errTime.TimeErrors
+		GeneralErrors = GeneralErrors
+		PaymentErrors = errPayment.PaymentErrors
 	)
 
 	allErrors := make([]error, 0)
 	allErrors = append(allErrors, GeneralErrors...)
-	allErrors = append(allErrors, FieldErrors...)
-	allErrors = append(allErrors, FieldScheduleErrors...)
-	allErrors = append(allErrors, TimeErrors...)
+	allErrors = append(allErrors, PaymentErrors...)
 
 	for _, item := range allErrors {
 		if err.Error() == item.Error() {
