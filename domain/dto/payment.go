66854f59cec7f26daa678ae0ba806d9aa8df3b12
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-08 10:58:34 +0700

    Add Payment DTOs and update structures for payment processing

---
+++ domain/dto/payment.go
@@ -0,0 +1,94 @@
+package dto
+
+import (
+	"payment-service/constants"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PaymentRequest struct {
+	PaymentLink    string          `json:"paymentLink"`
+	OrderID        string          `json:"orderID"`
+	ExpiredAt      time.Time       `json:"expiredAt"`
+	Amount         float64         `json:"amount"`
+	Description    *string         `json:"description"`
+	CustomerDetail *CustomerDetail `json:"customerDetail"`
+	ItemDetail     []ItemDetail    `json:"itemDetail"`
+}
+
+type CustomerDetail struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+type ItemDetail struct {
+	ID       string  `json:"id"`
+	Amount   float64 `json:"amount"`
+	Name     string  `json:"name"`
+	Quantity int     `json:"quantity"`
+}
+
+type PaymentRequestParam struct {
+	Page       int     `form:"page" validate:"required"`
+	Limit      int     `form:"limi" validate:"required"`
+	SortColumn *string `form:"sortColumn"`
+	SortOrder  *string `form:"sortOrder"`
+}
+
+type UpdatePaymentRequest struct {
+	TransactionID *string                 `json:"transactionID"`
+	Status        constants.PaymentStatus `json:"status"`
+	PaidAt        *time.Time              `json:"paidAt"`
+	VANumber      *string                 `json:"vaNumber"`
+	Bank          *string                 `json:"bank"`
+	InvoiceLink   *string                 `json:"invoiceLink,omitempty"`
+	Acquirer      *string                 `json:"acquirer"`
+}
+
+type PaymentResponse struct {
+	UUID          uuid.UUID                     `json:"uuid"`
+	OrderID       uuid.UUID                     `json:"orderID"`
+	Amount        float64                       `json:"amount"`
+	Status        constants.PaymentStatusString `json:"status"`
+	PaymentLink   string                        `json:"paymentLink"`
+	InvoiceLink   *string                       `json:"invoiceLink,omitempty"`
+	TransactionID *string                       `json:"transactionID"`
+	VANumber      *string                       `json:"vaNumber"`
+	Bank          *string                       `json:"bank"`
+	Acquirer      *string                       `json:"acquirer"`
+	Description   *string                       `json:"description"`
+	PaidAt        *time.Time                    `json:"paidAt"`
+	CreatedAt     *time.Time                    `json:"createdAt"`
+	UpdatedAt     *time.Time                    `json:"updatedAt"`
+}
+
+type Webhook struct {
+	VANumbers         []VANumber                    `json:"va_numbers"`
+	TransactionTime   string                        `json:"transaction_time"`
+	TransactionStatus constants.PaymentStatusString `json:"transaction_status"`
+	TransactionID     string                        `json:"transaction_id"`
+	StatusMessage     string                        `json:"status_message"`
+	StatusCode        string                        `json:"status_code"`
+	SignatureKey      string                        `json:"signature_key"`
+	SettlementTime    string                        `json:"settlement_time"`
+	PaymentType       string                        `json:"payment_type"`
+	PaymentAmount     []PaymentAmount               `json:"payment_amount"`
+	OrderID           uuid.UUID                     `json:"order_id"`
+	MerchantID        string                        `json:"merchant_id"`
+	GrossAmount       float64                       `json:"gross_amount"`
+	FraudStatus       string                        `json:"fraud_status"`
+	Currency          string                        `json:"currency"`
+	Acquirer          *string                       `json:"acquirer"`
+}
+
+type VANumber struct {
+	VaNumber string `json:"va_number"`
+	Bank     string `json:"bank"`
+}
+
+type PaymentAmount struct {
+	PaidAt *string `json:"paid_at"`
+	Amount *string `json:"amount"`
+}
