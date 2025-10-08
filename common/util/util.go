4e20f00bb1f39b7ab20a42c54e19678a89f3af94
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-08 09:55:57 +0700

    Add PDF generation functionality and helper function for template processing

--- common/util/util.go
+++ common/util/util.go
@@ -1,15 +1,18 @@
 package util
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"html/template"
 	"math"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
 
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"github.com/dustin/go-humanize"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -170,4 +173,46 @@ func BindFromConsul(dest any, endPoint, path string) error {
 	}
 
 	return nil
+}
+
+func add1(a int) int {
+	return a + 1
+}
+
+func GeneratePDFFromHTML(htmlTemplate string, data any) ([]byte, error) {
+	funcMap := template.FuncMap{
+		"add1": add1,
+	}
+
+	template, err := template.New("htmlTemplate").Funcs(funcMap).Parse(htmlTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	var filledTemplate bytes.Buffer
+	if err := template.Execute(&filledTemplate, data); err != nil {
+		return nil, err
+	}
+	htmlContent := filledTemplate.String()
+
+	pdfGenerator, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		logrus.Errorf("failed to create pdf generator: %v", err)
+		return nil, err
+	}
+
+	pdfGenerator.Dpi.Set(600)
+	pdfGenerator.NoCollate.Set(false)
+	pdfGenerator.Orientation.Set(wkhtmltopdf.OrientationPortrait)
+	pdfGenerator.PageSize.Set(wkhtmltopdf.PageSizeA4)
+	pdfGenerator.Grayscale.Set(false)
+	pdfGenerator.AddPage(wkhtmltopdf.NewPageReader(strings.NewReader(htmlContent)))
+
+	err = pdfGenerator.Create()
+	if err != nil {
+		logrus.Errorf("failed to create pdf: %v", err)
+		return nil, err
+	}
+
+	return pdfGenerator.Bytes(), err
 }
