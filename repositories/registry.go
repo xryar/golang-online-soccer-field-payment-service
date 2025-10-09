2fc6a614e9119dc4bdf991c0e794d55a3aae3864
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-09 11:53:40 +0700

    Implement registry for payment and payment history repositories

--- repositories/registry.go
+++ repositories/registry.go
@@ -1,1 +1,29 @@
 package repositories
+
+import (
+	paymentRepositories "payment-service/repositories/payment"
+	paymentHistoryRepositories "payment-service/repositories/paymentHistory"
+
+	"gorm.io/gorm"
+)
+
+type Registry struct {
+	db *gorm.DB
+}
+
+type IRegistryRepository interface {
+	GetPayment() paymentRepositories.IPaymentRepository
+	GetPaymentHistory() paymentHistoryRepositories.IPaymentHistoryRepository
+}
+
+func NewRegistryRepository(db *gorm.DB) IRegistryRepository {
+	return &Registry{db: db}
+}
+
+func (r *Registry) GetPayment() paymentRepositories.IPaymentRepository {
+	return paymentRepositories.NewPaymentRepository(r.db)
+}
+
+func (r *Registry) GetPaymentHistory() paymentHistoryRepositories.IPaymentHistoryRepository {
+	return paymentHistoryRepositories.NewPaymentHistory(r.db)
+}
