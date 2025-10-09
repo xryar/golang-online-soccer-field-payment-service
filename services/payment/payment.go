3319690988b5f097bf0c898315e9c41d030485b3
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-09 14:26:10 +0700

    Implement transaction handling in Create method of PaymentService with expiration validation

--- services/payment/payment.go
+++ services/payment/payment.go
@@ -3,11 +3,17 @@ package services
 import (
 	"context"
 	clients "payment-service/clients/midtrans"
+
 	"payment-service/common/gcs"
 	"payment-service/common/util"
+	errPayment "payment-service/constants/error/payment"
 	"payment-service/controllers/kafka"
 	"payment-service/domain/dto"
+	"payment-service/domain/models"
 	"payment-service/repositories"
+	"time"
+
+	"gorm.io/gorm"
 )
 
 type PaymentService struct {
@@ -94,6 +100,56 @@ func (ps *PaymentService) GetByUUID(ctx context.Context, uuid string) (*dto.Paym
 }
 
 func (ps *PaymentService) Create(ctx context.Context, req *dto.PaymentRequest) (*dto.PaymentResponse, error) {
+	var (
+		txErr, err error
+		payment    *models.Payment
+		response   *dto.PaymentResponse
+		midtrans   *clients.MidtransData
+	)
+
+	err = ps.repository.GetTx().Transaction(func(tx *gorm.DB) error {
+		if !req.ExpiredAt.After(time.Now()) {
+			return errPayment.ErrExpireAtInvalid
+		}
+
+		midtrans, txErr = ps.midtrans.CreatePaymentLink(req)
+		if txErr != nil {
+			return txErr
+		}
+
+		paymentRequest := &dto.PaymentRequest{
+			OrderID:     req.OrderID,
+			Amount:      req.Amount,
+			Description: req.Description,
+			ExpiredAt:   req.ExpiredAt,
+			PaymentLink: midtrans.RedirectURL,
+		}
+		payment, txErr := ps.repository.GetPayment().Create(ctx, tx, paymentRequest)
+		if txErr != nil {
+			return txErr
+		}
+
+		ps.repository.GetPaymentHistory().Create(ctx, tx, &dto.PaymentHistoryRequest{
+			PaymentID: payment.ID,
+			Status:    payment.Status.GetStatusString(),
+		})
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	response = &dto.PaymentResponse{
+		UUID:        payment.UUID,
+		OrderID:     payment.OrderID,
+		Amount:      payment.Amount,
+		Status:      payment.Status.GetStatusString(),
+		PaymentLink: payment.PaymentLink,
+		Description: payment.Description,
+	}
+
+	return response, nil
 }
 
 func (ps *PaymentService) Webhook(ctx context.Context, webhook *dto.Webhook) error {}
