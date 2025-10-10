74f61310036f49d02b3aa9e4e6bf35f834ce0fdc
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-10-10 14:56:01 +0700

    Implement PaymentController methods for handling requests and responses

--- controllers/http/payment/payment.go
+++ controllers/http/payment/payment.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
-	services "payment-service/services"
+	"net/http"
+	errValidation "payment-service/common/error"
+	"payment-service/common/response"
+	"payment-service/domain/dto"
+	"payment-service/services"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type PaymentController struct {
@@ -21,10 +26,145 @@ func NewPaymentController(service services.IRegistryService) IPaymentController 
 	return &PaymentController{service: service}
 }
 
-func (pc *PaymentController) GetAllWithPagination(*gin.Context) {}
+func (pc *PaymentController) GetAllWithPagination(ctx *gin.Context) {
+	var param dto.PaymentRequestParam
+	err := ctx.ShouldBindQuery(&param)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
 
-func (pc *PaymentController) GetByUUID(*gin.Context) {}
+		return
+	}
 
-func (pc *PaymentController) Create(*gin.Context) {}
+	validate := validator.New()
+	if err = validate.Struct(param); err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errValidation.ErrValidationResponse(err)
+		response.HttpResponse(response.ParamHTTPResponse{
+			Err:     err,
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Gin:     ctx,
+		})
 
-func (pc *PaymentController) Webhook(*gin.Context) {}
+		return
+	}
+
+	result, err := pc.service.GetPayment().GetAllWithPagination(ctx, &param)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusOK,
+		Data: result,
+		Gin:  ctx,
+	})
+
+}
+
+func (pc *PaymentController) GetByUUID(ctx *gin.Context) {
+	uuid := ctx.Param("uuid")
+	result, err := pc.service.GetPayment().GetByUUID(ctx, uuid)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusOK,
+		Data: result,
+		Gin:  ctx,
+	})
+}
+
+func (pc *PaymentController) Create(ctx *gin.Context) {
+	var request dto.PaymentRequest
+	err := ctx.ShouldBindJSON(request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	validate := validator.New()
+	if err = validate.Struct(request); err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errValidation.ErrValidationResponse(err)
+		response.HttpResponse(response.ParamHTTPResponse{
+			Err:     err,
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Gin:     ctx,
+		})
+
+		return
+	}
+
+	result, err := pc.service.GetPayment().Create(ctx, &request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusCreated,
+		Data: result,
+		Gin:  ctx,
+	})
+}
+
+func (pc *PaymentController) Webhook(ctx *gin.Context) {
+	var request dto.Webhook
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	err = pc.service.GetPayment().Webhook(ctx, &request)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusOK,
+		Gin:  ctx,
+	})
+}
--- controllers/http/registry.go
+++ controllers/http/registry.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	controllers "payment-service/controllers/http/payment"
+	"payment-service/services"
+)
+
+type Registry struct {
+	service services.IRegistryService
+}
+
+type IRegistryController interface {
+	GetPayment() controllers.IPaymentController
+}
+
+func NewRegistryController(service services.IRegistryService) IRegistryController {
+	return &Registry{service: service}
+}
+
+func (r *Registry) GetPayment() controllers.IPaymentController {
+	return controllers.NewPaymentController(r.service)
+}
